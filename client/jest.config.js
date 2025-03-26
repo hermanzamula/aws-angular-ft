6b14bd31f5328e3c78fa0a22e4458cd3208eb20d
Herman <herman.zamula@gmail.com>
2025-03-26 02:13:47 +0100

    Initial setup

---
+++ client/jest.config.js
@@ -0,0 +1,30 @@
+const esModules = ['@angular'].join('|')
+
+/**
+ * @type {import('@jest/types').Config.InitialOptions}
+ **/
+module.exports = {
+  transformIgnorePatterns: [`node_modules/(?!(${esModules})/)`],
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        tsconfig: './tsconfig.spec.json',
+        isolatedModules: true,
+        diagnostics: false,
+        allowSyntheticDefaultImports: true,
+      },
+    ],
+    '^.+\\.js$': 'babel-jest',
+    '^.+\\.mjs$': 'babel-jest', // Transform ES Modules with Babel
+  },
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: './coverage',
+  coverageProvider: 'v8',
+  testMatch: ['**/*.spec.ts'],
+  testEnvironment: 'jsdom',
+  coverageReporters: ['html'],
+  // setupFilesAfterEnv: ['<rootDir>/src/setup-jest.ts'],
+  moduleFileExtensions: ['ts', 'js', 'mjs', 'html', 'json'],
+}
---
+++ client/src/app/app.config.ts
@@ -0,0 +1,18 @@
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+import { provideStore } from '@ngxs/store';
+import { withNgxsReduxDevtoolsPlugin } from '@ngxs/devtools-plugin';
+import { withNgxsStoragePlugin } from '@ngxs/storage-plugin';
+import { withNgxsWebSocketPlugin } from '@ngxs/websocket-plugin';
+
+export const appConfig: ApplicationConfig = {
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideStore(
+[],
+), provideStore(
+[],
+withNgxsReduxDevtoolsPlugin(),
+withNgxsStoragePlugin(),
+withNgxsWebSocketPlugin())]
+};
